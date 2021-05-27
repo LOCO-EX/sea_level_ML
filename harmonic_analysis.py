a1553c2d6fc30342de2b5a9df439278f226a3518
Matias Duran Matute <mduranmatute@users.noreply.github.com>
2021-05-27 22:13:12 +0200

    Corrected one value

--- harmonic_analysis.py
+++ harmonic_analysis.py
@@ -44,6 +44,9 @@ years=[element.year for element in time]
 
 
 
+j2 = 473472
+
+level[j2]=(level[j2-1]+level[j2+1])/2
 # %% Perform harmonic analysis
 
 #import netCDF4
@@ -75,10 +78,46 @@ for year in np.unique(years):
     print(np.mean(hp))
     print(np.mean(level_tmp))
 
-
+d = {'time': time, 'sea_level': level, 'harmonic_rec': hp}
+df = pd.DataFrame(data=d)
 
 # %%
 #import matplotlib.pyplot as plt
 #plt.plot(time[0:800],level[0:800])
 #plt.plot(time[0:800],hp[0:800])
 #plt.show()
+
+# %% filtering the tide
+    
+from oceans.filters import lanc
+
+freq = 1./40/6  # Hours
+window_size = 6*(96+1+96)
+pad = np.zeros(window_size) * np.NaN
+
+wt = lanc(window_size, freq)
+res = np.convolve(wt, df['sea_level']-694.0, mode='same')
+
+df['low'] = res
+df['high'] = df['sea_level'] - df['low']
+
+df['pandas_l'] = df['sea_level'].rolling( window=240, center=True).mean()
+df['pandas_h'] = df['sea_level'] - df['pandas_l']
+
+# %% This part using iris uses too much memory
+
+#import iris
+#from iris.pandas import as_cube
+
+#cube = as_cube(df['sea_level'])
+#low = cube.rolling_window('index',
+#                        iris.analysis.SUM,
+#                        len(wt),
+#                        weights=wt)
+
+#df['iris_l'] = np.r_[pad, low.data, pad]
+#df['iris_h'] = df['sea_level'] - df['iris_l']
+
+
+# %%
+df.to_csv('data/SL_DH_decomposed.csv')
