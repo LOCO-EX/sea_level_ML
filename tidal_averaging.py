f2be3ee1888ce862b9ea6de05c4c745bdb8fcfca
Matias Duran Matute <mduranmatute@users.noreply.github.com>
2021-06-10 14:53:41 +0200

    added a few plots

--- tidal_averaging.py
+++ tidal_averaging.py
@@ -11,6 +11,7 @@ Created on Sat May 29 06:36:13 2021
 import pandas as pd
 import time
 import numpy as np
+import matplotlib.pyplot as plt
 
 # %% Load data
 SL = pd.read_csv('data/SL_DH_decomposed.csv')
@@ -48,10 +49,31 @@ def isInt(num):
 
 int_p = np.where(isInt(periods))[0]
 
-level_av = [np.trapz(level_i[int_p[i]:int_p[i+1]],dx=dT) for i in np.arange(len(int_p)-1)]
-hrec_av  = [np.trapz(hrec_i[int_p[i]:int_p[i+1]],dx=dT) for i in np.arange(len(int_p)-1)]
-res_av   = [np.trapz(res_i[int_p[i]:int_p[i+1]],dx=dT) for i in np.arange(len(int_p)-1)]
+level_av = [np.trapz(level_i[int_p[i]:int_p[i+1]+1],dx=dT) for i in np.arange(len(int_p)-1)]
+hrec_av  = [np.trapz(hrec_i[int_p[i]:int_p[i+1]+1],dx=dT) for i in np.arange(len(int_p)-1)]
+res_av   = [np.trapz(res_i[int_p[i]:int_p[i+1]+1],dx=dT) for i in np.arange(len(int_p)-1)]
 
+# %%Plot some visual checks
+
+t_lim = 268272.0
+
+plt.plot(t[t<t_lim],level[t<t_lim],'r')
+plt.plot(t_i[t_i<t_lim],level_i[t_i<t_lim],'--g')
+plt.plot(t_i[int_p[0:7]],level_i[int_p[0:7]],'x')
+plt.plot((t_i[int_p[0:7]]+t_i[int_p[1:8]])/2,level_av[0:7],'o-')
+plt.show()
+
+plt.plot(t[t<t_lim],res[t<t_lim],'r')
+plt.plot(t_i[t_i<t_lim],res_i[t_i<t_lim],'--g')
+plt.plot(t_i[int_p[0:7]],res_i[int_p[0:7]],'x')
+plt.plot((t_i[int_p[0:7]]+t_i[int_p[1:8]])/2,res_av[0:7],'o-')
+plt.show()
+
+plt.plot(t[t<t_lim],hrec[t<t_lim],'r')
+plt.plot(t_i[t_i<t_lim],hrec_i[t_i<t_lim],'--g')
+plt.plot(t_i[int_p[0:7]],hrec_i[int_p[0:7]],'x')
+plt.plot((t_i[int_p[0:7]]+t_i[int_p[1:8]])/2,hrec_av[0:7],'o-')
+plt.show()
 
 # %%
 
@@ -69,9 +91,9 @@ a_y = np.cos(np.pi/180*a)
 a_x[a == 990] = np.nan;
 a_y[a == 990] = np.nan;
 
-mean_a_x = np.zeros([len(int_p),1])
-mean_a_y = np.zeros([len(int_p),1])
-trap_v   = np.zeros([len(int_p),1])
+mean_a_x = np.zeros([len(int_p)-1,])
+mean_a_y = np.zeros([len(int_p)-1,])
+trap_v   = np.zeros([len(int_p)-1,])
 
 for i in np.arange(1,len(int_p)-1):
 
@@ -99,4 +121,31 @@ for i in np.arange(1,len(int_p)-1):
 #W_EX_av = np.array([np.trapz(W_EX_i[int_p[i]:int_p[i+1]],dx=dT) for i in np.arange(len(int_p)-1)])
 
 #a_mean = 180/np.pi*np.arctan(mean_a_x/mean_a_y)
-a_mean = 180/np.pi*np.arctan2(mean_a_x,mean_a_y)
+a_mean = -180/np.pi*np.arctan2(mean_a_x,mean_a_y)
+
+
+# %% Plot results
+plt.scatter(a_mean,np.array(level_av),c=trap_v)
+plt.show()
+
+plt.scatter(a_mean,hrec_av,c=trap_v)
+plt.show()
+
+plt.scatter(a_mean,res_av,c=trap_v)
+plt.show()
+
+# %% Plot correlations
+
+from scipy.stats import gaussian_kde
+
+mask = np.logical_or(a_mean<90, a_mean>45)
+
+tmp= np.array(res_av)
+
+trap_v[np.where(np.isnan(trap_v))]=0
+
+xy = np.vstack([trap_v[mask],tmp[mask]])
+z = gaussian_kde(xy)(xy)
+
+plt.scatter(trap_v[mask]/12,(tmp[mask]-690)**(1/2),c=z)
+plt.show()
