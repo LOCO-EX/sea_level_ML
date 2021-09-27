642fd5bba3a7ade82b5108f6f7e1f69415dd20d8
Matias Duran Matute <mduranmatute@users.noreply.github.com>
2021-09-27 16:55:41 +0200

    Corrected number of features

--- LSTM_forecast.py
+++ LSTM_forecast.py
@@ -68,7 +68,7 @@ scaled = scaler.fit_transform(values)
 # frame as supervised learning
 # specify the number of lag hours
 n_periods = 1
-n_features = 5 # Is this correct?
+n_features = 3 # This is the number of features used to predict
 # frame as supervised learning
 reframed = series_to_supervised(scaled, n_periods, 1)
 # drop columns we don't want to predict
@@ -78,7 +78,7 @@ print(reframed.head())
 # %%
 # split into train and test sets
 values = reframed.values
-n_train_periods = 5000
+n_train_periods = 2500
 train = values[:n_train_periods, :]
 test = values[n_train_periods:, :]
 # split into input and outputs
@@ -95,7 +95,7 @@ print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
 #%%
 # design network
 model = Sequential()
-model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2])))
+model.add(LSTM(10, input_shape=(train_X.shape[1], train_X.shape[2])))
 model.add(Dense(1))
 model.compile(loss='mae', optimizer='adam')
 # fit network
@@ -107,3 +107,17 @@ pyplot.legend()
 pyplot.show()
 
 # %% Make a prediction
+yhat = model.predict(test_X)
+test_X = test_X.reshape((test_X.shape[0], n_periods*n_features))
+# invert scaling for forecast
+inv_yhat = concatenate((yhat, test_X[:, -7:]), axis=1)
+inv_yhat = scaler.inverse_transform(inv_yhat)
+inv_yhat = inv_yhat[:,0]
+# invert scaling for actual
+test_y = test_y.reshape((len(test_y), 1))
+inv_y = concatenate((test_y, test_X[:, -4:]), axis=1)
+inv_y = scaler.inverse_transform(inv_y)
+inv_y = inv_y[:,0]
+# calculate RMSE
+rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+print('Test RMSE: %.3f' % rmse)
